ce18ee897db2708178f3c2d84388000245b9df45
ibouzidi <bouzidi.idris42@gmail.com>
2022-06-20 17:21:41 +0200

    add model city and display the weather from the cities stored in the database.

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,4 @@
+from .models import City
 from django.shortcuts import render
 import requests
 import datetime
@@ -6,23 +7,28 @@ import datetime
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric' \
           '&lang=fr&appid=83e4a44c491d71a05757e7679b2b9f65'
-    city = 'Saint-Étienne'
-    city_weather = requests.get(url.format(city)).json()
+    weather_data = []
 
     def get_date(timezone):
         tz = datetime.timezone(datetime.timedelta(seconds=int(timezone)))
         return datetime.datetime.now(tz=tz).strftime("%m/%d/%Y, %H:%M")
 
-    weather = {
-        'city': f'{city}',
-        'country': f'{city_weather["sys"]["country"]}',
-        'temperature': f'{city_weather["main"]["temp"]}',
-        'description': f'{city_weather["weather"][0]["description"]}',
-        'icon': f'{city_weather["weather"][0]["icon"]}',
-        'wind_speed': f'{city_weather["wind"]["speed"]}',
-        'humidity': f'{city_weather["main"]["humidity"]}',
-        'timezone': f'{get_date(city_weather["timezone"])}'
-    }
-    context = {'weather': weather}
-    print(city_weather)
+    cities = City.objects.all()
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+        weather = {
+            'city': f'{city}',
+            'country': f'{city_weather["sys"]["country"]}',
+            'temperature': f'{round(city_weather["main"]["temp"], 1)}',
+            'description': f'{city_weather["weather"][0]["description"]}',
+            'icon': f'{city_weather["weather"][0]["icon"]}',
+            'wind_speed': f'{city_weather["wind"]["speed"]}',
+            'humidity': f'{city_weather["main"]["humidity"]}',
+            'timezone': f'{get_date(city_weather["timezone"])}'
+        }
+        weather_data.append(weather)
+    print("weather_data")
+    print(weather_data)
+    context = {'weather_date': weather_data}
+    # print(city_weather)
     return render(request, 'weather/index.html', context)
